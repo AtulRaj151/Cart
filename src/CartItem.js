df1fc99bce190261c5592ae9ad60219330f6b245
Atul Raj <atulsid151@gmail.com>
2020-06-17 16:44:00 +0530

    add git qty status

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+             price:1000,
+             title:"Mobile Phones",
+             qty: 1,
+             img:''
+        }
+        // this.increaseQuantity = this.increaseQuantity.bind(this);
+    }
+    increaseQuantity = () =>{
+        console.log('this',this);
+    }
        render() {
-
+        const {price,qty,title } = this.state;
            return (
                <div className="cart-item">
 
@@ -11,12 +24,27 @@ class CartItem extends React.Component {
                      </div>
 
                      <div className="right-block">
-                         <div style={ {fontSize:25 } }>Phone</div>
-                         <div  style={ { color:"#777" } }>Rs. 1999</div>
-                         <div  style={ { color:"#777" } }>Qty. 1</div>
+                         <div style={ {fontSize:25 } }>{ title}</div>
+                         <div  style={ { color:"#777" } }>Rs. {price}</div>
+                         <div  style={ { color:"#777" } }>Qty. {qty}</div>
 
                          <div className="cart-item-actions">
-                                {/* Buttons */}
+                         
+                                <img 
+                                alt="increase" 
+                                className="action-icons"
+                                src="https://image.flaticon.com/icons/svg/992/992651.svg"
+                                onClick={this.increaseQuantity}
+
+                                 />
+                                <img alt="decrease"
+                                className="action-icons"
+                                src="https://image.flaticon.com/icons/svg/864/864373.svg"
+                                 />
+                                <img alt="delete" 
+                                className="action-icons" 
+                                src="https://image.flaticon.com/icons/svg/1345/1345823.svg" 
+                                />
 
                          </div>
 
@@ -31,7 +59,7 @@ class CartItem extends React.Component {
 
 const Style = {
     image:{
-        height:100,
+        height:120,
         width:110,
         borderRadius:4,
         background:"#ccc"
